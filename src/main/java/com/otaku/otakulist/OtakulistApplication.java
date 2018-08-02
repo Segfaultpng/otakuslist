4467844d9f7612271d56015401128c5acaf25a2d
Stephen P <stephenp@Zipcoders-MacBook-Pro-79.local>
2018-08-02 12:31:16 -0400

    updating

---
+++ src/main/java/com/otaku/otakulist/OtakulistApplication.java
@@ -0,0 +1,12 @@
+package com.otaku.otakulist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtakulistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtakulistApplication.class, args);
+	}
+}
---
+++ src/test/java/com/otaku/otakulist/OtakulistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.otaku.otakulist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtakulistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
